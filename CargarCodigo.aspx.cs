d9e3f73ea9ffbb3a8b75bdac58a753bd06b5968d
Jess Ibañez <jessicavanina18@gmail.com>
2024-10-02 22:54:54 -0300

    Función Validación de Voucher
    En proceso

--- CargarCodigo.aspx.cs
+++ CargarCodigo.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,39 @@ namespace Tp_PromoWeb_Equipo_4A
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            string codigoVoucher = Request.Form["codigo"];
+
+            if (ValidarCodigoVoucher(codigoVoucher))
+            { 
+                Response.Redirect("SeleccionPremio.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('El código es inválido o ya ha sido utilizado.');</script>");
+            }
+        }
+
+        private bool ValidarCodigoVoucher(string codigo)
+        {
+            bool codigoEsValido = false;
+
+            string connectionString = "PROMOS_DB";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Vouchers WHERE CodigoVoucher = @CodigoVoucher AND FechaCanje IS NULL";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@CodigoVoucher", codigo);
+
+                conn.Open();
+                int count = (int)cmd.ExecuteScalar();
+                codigoEsValido = count > 0;
+            }
+
+            return codigoEsValido;
         }
     }
 }
