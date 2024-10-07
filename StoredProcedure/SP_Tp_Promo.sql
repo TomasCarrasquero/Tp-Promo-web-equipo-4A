e3f32128fd6d2a74b9c04643bcef58f96b6745c2
Tomas Carrasquero <cardenalthompson@hotmail.com>
2024-10-06 21:23:41 -0300

    ahora si procedure

--- StoredProcedure/SP_Tp_Promo.sql
+++ StoredProcedure/SP_Tp_Promo.sql
@@ -1,27 +1,27 @@
 
 --Buscar Cliente por DNI
 
-CREATE PROCEDURE storedProcedureBuscarClientePorDNI
+CREATE OR ALTER PROCEDURE storedProcedureBuscarClientePorDNI
     @documento VARCHAR(50)
 AS
 BEGIN
     SELECT Id, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP 
     FROM Clientes 
-    WHERE Documento = @documento
+    WHERE Documento = @documento
 END
 
 GO
 
 -- Listar Clientes
 
-CREATE PROCEDURE storedProcedureListarClientes AS
-SELECT Id, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP FROM Clientes
+CREATE OR ALTER PROCEDURE storedProcedureListarClientes AS
+SELECT Id, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP FROM Clientes
 
 GO
 
 -- Alta cliente 
 
-CREATE PROCEDURE storedAltaCliente
+CREATE OR ALTER PROCEDURE storedAltaCliente
 @documento varchar(50),
 @nombre varchar(50),
 @apellido varchar(50),
@@ -35,12 +35,30 @@ INSERT INTO Clientes VALUES(@documento, @nombre, @apellido, @email, @direccion, 
 GO
 
 -- listar vouchers
+
 CREATE OR ALTER PROCEDURE storedProcedureListarCodigo AS
 select * from Vouchers
 
 GO
 
 -- listar Articulos
- CREATE OR ALTER PROCEDURE storedProcedureListar AS
- SELECT A.Id ,Codigo, Nombre, A.Descripcion, M.Descripcion AS 'Marca', M.Id, C.Descripcion AS 'Categoría', C.Id, Precio FROM ARTICULOS A, MARCAS M , CATEGORIAS C where M.Id = A.IdMarca and A.IdCategoria = c.Id
- GO
+
+CREATE OR ALTER PROCEDURE storedProcedureListar AS
+BEGIN
+SELECT A.Id ,Codigo, Nombre, A.Descripcion, M.Descripcion AS 'Marca', C.Descripcion AS 'Categoría', Precio 
+FROM ARTICULOS A, MARCAS M , CATEGORIAS C where M.Id = A.IdMarca and A.IdCategoria = c.Id
+END
+
+GO
+
+-- modificar vouchers
+CREATE OR ALTER PROCEDURE soredProcedureModificarVoucher 
+    @CodigoVoucher VARCHAR(50),
+    @IdCliente int,
+    @FechaCanje DATE,
+    @IdArticulo int
+AS
+BEGIN
+UPDATE Vouchers SET IdCliente = @IdCliente, FechaCanje = @FechaCanje, IdArticulo = @IdArticulo
+where CodigoVoucher = @CodigoVoucher
+END
